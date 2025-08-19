0ad317204c031a3e140176a1c72cb7066b6825ef
Ienciu-Bocse <uih04503@contiwan.com>
2025-08-19 15:54:13 +0300

    redenumire folder server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta principală – trimite index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'SkilsUX', 'Index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'Index.html'));
 });
 
 // Portul corect
